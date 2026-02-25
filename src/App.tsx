1d8f6d2d361909251b0445d662512c98de138351
arjunRao <arjukrao2004@gmail.com>
2026-02-25 11:16:17 +0530

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-import AnimatedCursor from 'react-animated-cursor';
 import Loader from './components/Loader';
 import SceneLayout from './layout/SceneLayout';
 import Experience from './components/Experience';
@@ -23,46 +22,32 @@ const App: React.FC = () => {
 
   return (
     <>
-     <div className="hidden md:block">
-       <AnimatedCursor
-        innerSize={8}
-        outerSize={35}
-        color="255, 255, 255"
-        outerAlpha={0.2}
-        innerScale={1}
-        outerScale={2}
-        clickables={['a', 'button', '.link', '[role="button"]']}
-
-      />
-     </div>
-     
-
-          <SceneLayout>
-      <div className="text-white font-sans scroll-smooth h">
+      <SceneLayout>
+        <div className="text-white font-sans scroll-smooth h">
           <Navbar />
-        <main>
-          <section id="hero">
-            <Hero />
-          </section>
-          <section id="about" className="">
-            <About />
-          </section>
-          <section id="experience" className="">
-            <Experience />
-          </section>
-          <section id="projects" className="">
-            <Projects />
-          </section>
-          <section id="skills" className="">
-            <Skills />
-          </section>
-          <section id="contact" className="">
-            <Contact />
-          </section>
-        </main>
-        <Footer />
-      </div>
-          </SceneLayout>
+          <main>
+            <section id="hero">
+              <Hero />
+            </section>
+            <section id="about" className="">
+              <About />
+            </section>
+            <section id="experience" className="">
+              <Experience />
+            </section>
+            <section id="projects" className="">
+              <Projects />
+            </section>
+            <section id="skills" className="">
+              <Skills />
+            </section>
+            <section id="contact" className="">
+              <Contact />
+            </section>
+          </main>
+          <Footer />
+        </div>
+      </SceneLayout>
     </>
   );
 };
